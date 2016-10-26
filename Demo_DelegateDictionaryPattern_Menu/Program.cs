c47b2885c0633dc2c31aa4b8d8f18a8615e84f3f
John Velis <john@johnvelis.com>
2016-10-26 05:50:55 -0400

    commit 550

--- Demo_DelegateDictionaryPattern_Menu/Program.cs
+++ Demo_DelegateDictionaryPattern_Menu/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Demo_DelegateDictionaryPattern_Menu
@@ -18,15 +19,35 @@ namespace Demo_DelegateDictionaryPattern_Menu
 
         static void Main(string[] args)
         {
-
             Dictionary<MenuOption, Action> menuActionMap = new Dictionary<MenuOption, Action>();
 
             menuActionMap.Add(MenuOption.DisplayName, DisplayAddress);
             menuActionMap.Add(MenuOption.DisplayAddress, DisplayName);
             menuActionMap.Add(MenuOption.Quit, Quit);
 
+            MenuOption menuChoice = MenuOption.None;
 
+            menuChoice = DisplayGetMenuChoice();
 
+            if (menuActionMap.ContainsKey(menuChoice))
+                menuActionMap[menuChoice]();
+            else
+                throw new Exception("Menu choice not valid.");
+
+        }
+
+        private static MenuOption DisplayGetMenuChoice()
+        {
+            MenuOption menuChoice = MenuOption.None;
+
+            DisplayHeader("Main Menu");
+
+            foreach (MenuOption menuItem in Enum.GetValues(typeof(MenuOption)))
+            {
+                Console.WriteLine(ToLabelFormat(menuItem.ToString()));
+            }
+
+            return menuChoice;
         }
 
         private static void Quit()
@@ -36,21 +57,56 @@ namespace Demo_DelegateDictionaryPattern_Menu
 
         private static void DisplayName()
         {
+            DisplayHeader("Name");
+
             Console.WriteLine();
             Console.WriteLine("John E Velis");
             Console.WriteLine();
             Console.WriteLine("Press any key to continue.");
-            Console.ReadKey();
+
+            DisplayContinuePrompt();
         }
 
         private static void DisplayAddress()
         {
+            DisplayHeader("Address");
+
             Console.WriteLine();
             Console.WriteLine("6625 Echo Valley Road");
             Console.WriteLine("Empire, MI 49630");
             Console.WriteLine();
             Console.WriteLine("Press any key to continue.");
+
+            DisplayContinuePrompt();
+        }
+
+        private static void DisplayContinuePrompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+        }
+
+        private static void DisplayHeader(string headerText)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(headerText);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// convert camel-case to all upper case and spaces
+        /// reference URL - http://stackoverflow.com/questions/15458257/how-to-have-enum-values-with-spaces
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static String ToLabelFormat(String s)
+        {
+            var newStr = Regex.Replace(s, "(?<=[A-Z])(?=[A-Z][a-z])", " ");
+            newStr = Regex.Replace(newStr, "(?<=[^A-Z])(?=[A-Z])", " ");
+
+            return newStr;
         }
     }
 }
